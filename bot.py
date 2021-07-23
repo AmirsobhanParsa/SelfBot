39b822a61928d4270f587c123824224b9ae086f7
AmirsobhanParsa <60932777+AmirsobhanParsa@users.noreply.github.com>
2021-07-23 22:40:30 +0430

    Add Files

---
+++ bot.py
@@ -0,0 +1,10 @@
+import time
+from pyrogram import Client,filters
+bot = Client(session_name='auto',config_file='config.ini')
+@bot.on_message(filters.regex('')&filters.private)
+async def start(client,message):
+    chat_id = message.chat.id
+    #await bot.send_message(chat_id,"سلام ، این پیام بصورت خودکار توسط ربات برای شما ارسال شده است. لطفا منتظر پاسخگویی باشید.")
+    await bot.send_message(chat_id,"سلام ، این پیام بصورت خودکار توسط ربات برای شما ارسال شده است. لطفا منتظر پاسخگویی باشید.")
+bot.run()
+    
