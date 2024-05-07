88ed6c4ece5b539d2265221113b238ce287486b0
AmirsobhanParsa <60932777+AmirsobhanParsa@users.noreply.github.com>
2024-05-07 16:54:02 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ bot = Client(session_name='auto',config_file='config.ini')
 @bot.on_message(filters.regex('')&filters.private)
 async def start(client,message):
     chat_id = message.chat.id
-    #await bot.send_message(chat_id,"سلام ، این پیام بصورت خودکار توسط ربات برای شما ارسال شده است. لطفا منتظر پاسخگویی باشید.")
     await bot.send_message(chat_id,"سلام ، این پیام بصورت خودکار توسط ربات برای شما ارسال شده است. لطفا منتظر پاسخگویی باشید.")
 bot.run()
     
